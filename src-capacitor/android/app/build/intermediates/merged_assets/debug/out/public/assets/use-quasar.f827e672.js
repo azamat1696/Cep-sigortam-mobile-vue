5304e81e6fadb5020e3d212f8c8cc221305f915b
Azamat <73704375+azamat1696@users.noreply.github.com>
2023-05-18 16:08:01 +0300

    update src

---
+++ src-capacitor/android/app/build/intermediates/merged_assets/debug/out/public/assets/use-quasar.f827e672.js
@@ -0,0 +1,1 @@
+import{i as a,ax as r}from"./index.30f5c437.js";function u(){return a(r)}export{u};
---
+++ src-capacitor/android/app/build/intermediates/merged_assets/release/out/public/assets/use-quasar.9930c979.js
@@ -0,0 +1,1 @@
+import{i as a,ax as r}from"./index.514420a3.js";function u(){return a(r)}export{u};
